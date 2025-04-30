cb87b3a61b5ba49948f489041cfc7318b7a0c036
Leontev-E <110973495+Leontev-E@users.noreply.github.com>
2025-04-30 09:09:49 +0400

    Add database connection logging ER_NO_DB_ERROR

--- db.js
+++ db.js
@@ -2,20 +2,13 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-pool.on('acquire', () => {
-    console.log('Connection acquired');
-});
-pool.on('release', () => {
-    console.log('Connection released');
-});
-
 const pool = mysql.createPool({
     host: 'evgenl9s.beget.tech',
     user: 'evgenl9s_klm_wik',
     password: 'ATKPamOM6!r&',
     database: 'evgenl9s_klm_wik',
     waitForConnections: true,
-    connectionLimit: 20,
+    connectionLimit: 10,
     queueLimit: 0,
     charset: 'utf8mb4'
 });
